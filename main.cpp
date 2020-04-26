b1e1f6b81786dd8e694cea1865619a64bff16d77
김윤지 <64121179+wayuj@users.noreply.github.com>
2020-04-26 16:18:07 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -39,7 +39,7 @@ int main(){
 					if(p->Delete(tmpItem)==true)
 						cout<<tmpItem<<"가 삭제되었습니다."<<endl;
 						
-					else cout<<"비어있습니다. 삭제 실패"<<endl;
+					else cout<<"비어있습니다. 삭제 실패!"<<endl;
 					break;
 					
 				case 3:
